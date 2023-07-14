02bc16eb22e440fdead5106b8a41bca9d08289ff
Paweł Zając <pavlzayac@gmail.com>
2023-07-14 16:35:04 +0200

    Update sidenav buttons

--- components/sidenav/SideNav.tsx
+++ components/sidenav/SideNav.tsx
@@ -1,22 +1,22 @@
 import { IconContext } from "react-icons"
 import SideNavButton from "./SideNavButton"
-import { MdCalendarToday } from "react-icons/md"
+import { MdCalendarToday, MdGroup, MdLockClock, MdOutlineCalendarToday, MdOutlineGroup, MdOutlineLockClock, MdOutlineRule, MdOutlineSettings, MdPerson, MdPerson2, MdPerson3, MdRule, MdSettings } from "react-icons/md"
 
 const iconSize = "22"
 
 const buttons = [
-    { name: 'Bruh', iconComponent: <MdCalendarToday size={iconSize} /> },
-    { name: 'Bruh', iconComponent: <MdCalendarToday size={iconSize} /> },
-    { name: 'Bruh', iconComponent: <MdCalendarToday size={iconSize} /> },
-    { name: 'Bruh', iconComponent: <MdCalendarToday size={iconSize} /> },
-    { name: 'Bruh', iconComponent: <MdCalendarToday size={iconSize} /> },
+    { name: 'Bruh1', iconComponent: <MdOutlineCalendarToday size={iconSize} /> },
+    { name: 'Bruh2', iconComponent: <MdOutlineGroup size={iconSize} /> },
+    { name: 'Bruh3', iconComponent: <MdOutlineLockClock size={iconSize} /> },
+    { name: 'Bruh4', iconComponent: <MdOutlineRule size={iconSize} /> },
+    { name: 'Bruh5', iconComponent: <MdOutlineSettings size={iconSize} /> },
 ]
 
 export default function SideNav() {
     return (
-        <div className="fixed top-0 left-0 w-64 h-screen flex-col bg-light-500">
+        <div className="fixed top-0 left-0 h-screen flex-col bg-light-500 w-56">
             {buttons.map(button => (
-                <SideNavButton name={button.name} iconComponent={button.iconComponent} />
+                <SideNavButton key={button.name} name={button.name} iconComponent={button.iconComponent} />
             ))}
         </div>
     )
--- components/sidenav/SideNavButton.tsx
+++ components/sidenav/SideNavButton.tsx
@@ -1,8 +1,8 @@
 export default function SideNavButton({ name, iconComponent }: { name: string, iconComponent: React.ReactNode }) {
     return (
-        <div className="flex justify-center items-center bg-purple-300 px-10 py-5 mx-5 my-3 rounded-xl">
-            <div className="flex-1 mr-auto">{iconComponent}</div>
-            <span className="flex-1">{name}</span>
+        <div className="flex hover:cursor-pointer hover:bg-purple-200 text-gray-700 hover:text-black px-10 py-5 mx-5 my-0 rounded-full">
+            <div>{iconComponent}</div>
+            <span className="font-medium ml-4">{name}</span>
         </div>
     )
 
